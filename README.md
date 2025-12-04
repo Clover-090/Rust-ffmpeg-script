a7122dff06130542e850e40f7dc4d228814fa4fd
Clover-090 <Clover.c21@proton.me>
2025-12-04 18:39:17 -0500

    Implemented using std::path to manipulate the infile and outfile, main functionality works with ffmpeg on my system. This was a fun excersize.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A rust program that searches through all subdirectories in a given directory and converts specified media formats into a diferent specified format.
 This code is, and has always been, written 100% by humans. No generative AI has been or ever will be used to write code in this repo, genAI is only possible by stealing the hard work of other developer so not only is it unehtical to use, it is impossible to create code with it that can exist an run under any license. it creates more problems and work than it prevents, is a huge security risk, and is ultimately a lousey replacement for actually skill and effort.
 
-# Needs FFmpeg 7.0 or higher
+# Needs FFmpeg 7.0 or higher 
+liblamemp3 and other external libraries are your resposibility, documentation can be found on thr FFMPEG website and on various forums
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use walkdir::WalkDir;
 use std::io;
+use std::process::Command;
+
 
 //what it says on the tin, removes newlines from variables that store user inputs
 fn newlineremover(x: &mut String) {
@@ -13,7 +15,10 @@ fn main() {
     //Declairing variables for future use
     let mut searchdir = String::new();
     let mut extention1 = String::new();
-    //let mut extention2 = String::new();
+    let program = "ffmpeg";
+    let mut extention2 = String::new();
+
+
 
     loop{
 
@@ -25,19 +30,31 @@ fn main() {
         println!("Enter the file extention to search for\n");
 
         io::stdin().read_line(&mut extention1).expect("Failed to read line");
+
+        println!("Enter file extention to convert to, make sure not put a period infront of it, you want mp3 not .mp3\n");
         
+        io::stdin().read_line(&mut extention2).expect("Failed to read line");
+
         //Remove newline from the end
         newlineremover(&mut searchdir);
         newlineremover(&mut extention1);
-
+        newlineremover(&mut extention2);
+        
         //Currently prints the path of all the files that are under the given directory and all its sub directories, skips files that it doesn not have perms for
         for file in WalkDir::new(&searchdir).into_iter().filter_map(|e| e.ok()) {
-            //makes sure what its looking at is a file
-            if file.metadata().unwrap().is_file(){
-                //turns the filename to a string and checks its extention
-                if file.file_name().to_string_lossy().ends_with(&extention1) {
-                    println!("{}", file.path().display());
-                }
+            //Infile for the FFMPEG command
+            let inpath = file.path();
+
+            let mut out_path = inpath.to_path_buf();
+            
+            out_path.set_extension(&extention2);
+
+
+            let ffmpeg = Command::new(program).arg("-i").arg(inpath).arg(out_path).output();
+
+            //makes sure what its looking at is a file and turns the filename to a string and checks its extention
+            if file.metadata().expect("Can't read file metadata").is_file() && file.file_name().to_string_lossy().ends_with(&extention1){
+                println!("status: {:?}", ffmpeg)
             }
         }
     }
